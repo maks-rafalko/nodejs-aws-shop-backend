bd583146be9e5a9fe454c6cd62619857dce34dce
Rafalko, Maksim <maks.rafalko@gmail.com>
2023-05-31 15:10:26 +0300

    refactor: remove not needed mocks, resolve TS errors

--- product-microservice/lambdas/mocks/data.ts
+++ product-microservice/lambdas/mocks/data.ts
@@ -1,7 +1,4 @@
-import { CartItem } from "../models/CartItem";
-import { Order } from "../models/Order";
 import { AvailableProduct, Product } from "../models/Product";
-import { OrderStatus } from "../constants/order";
 
 export const products: Product[] = [
   {
@@ -45,60 +42,3 @@ export const products: Product[] = [
 export const availableProducts: AvailableProduct[] = products.map(
   (product, index) => ({ ...product, count: index + 1 })
 );
-
-export const cart: CartItem[] = [
-  {
-    product: {
-      description: "Short Product Description1",
-      id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
-      price: 24,
-      title: "ProductOne",
-    },
-    count: 2,
-  },
-  {
-    product: {
-      description: "Short Product Description7",
-      id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
-      price: 15,
-      title: "ProductName",
-    },
-    count: 5,
-  },
-];
-
-export const orders: Order[] = [
-  {
-    id: "1",
-    address: {
-      address: "some address",
-      firstName: "Name",
-      lastName: "Surname",
-      comment: "",
-    },
-    items: [
-      { productId: "7567ec4b-b10c-48c5-9345-fc73c48a80aa", count: 2 },
-      { productId: "7567ec4b-b10c-45c5-9345-fc73c48a80a1", count: 5 },
-    ],
-    statusHistory: [
-      { status: OrderStatus.Open, timestamp: Date.now(), comment: "New order" },
-    ],
-  },
-  {
-    id: "2",
-    address: {
-      address: "another address",
-      firstName: "John",
-      lastName: "Doe",
-      comment: "Ship fast!",
-    },
-    items: [{ productId: "7567ec4b-b10c-48c5-9345-fc73c48a80aa", count: 3 }],
-    statusHistory: [
-      {
-        status: OrderStatus.Sent,
-        timestamp: Date.now(),
-        comment: "Fancy order",
-      },
-    ],
-  },
-];
